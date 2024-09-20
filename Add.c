d60d9ee89caa95d35ec2992dc2dd0c5e4c0f4a05
Meghanayenni <meghanayenni24@gmail.com>
2024-09-20 13:38:05 +0530

    Update Add.c

--- Add.c
+++ Add.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main(){
-int a,b,c;
-int a=5;
-int b=3;
-printf("The sum of a and b is %d",c)
+  int a,b,c;
+  int a=5;
+  int b=3;
+  printf("The sum of a and b is %d",c);
+  return 0;
 }
